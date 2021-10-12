9e8431e1960b29d601055ca47a71041e071e636b
Rosarindaferani <rosarindaferani@gmail.com>
2021-10-12 16:24:49 +0700

    first commit

---
+++ JavaBasic.java
@@ -0,0 +1,15 @@
+public class JavaBasic {
+    public static void main(String[] args){
+        String firstName ="FirstName:Rosarinda";
+        String lastName ="LastName:Ferani";
+        String age ="Age:22";
+        String address = "Address:Batam,Nongsa";
+        String phoneNumber = "Phone Number :081365215956";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(age);
+        System.out.println(address);
+        System.out.println(phoneNumber);
+    }
+}
